a347e6ae80b30f6b77bd9b909c68dbe2600bb10b
Shelepov Anton <antonsh1@mail.ru>
2023-03-01 23:45:37 +0500

    Перемещен и переименован класс тестов

--- src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
@@ -1,5 +1,7 @@
-import org.junit.jupiter.api.*;
-import org.mockito.ArgumentCaptor;
+package ru.netology.patient.service.medical;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.BloodPressure;
 import ru.netology.patient.entity.HealthInfo;
@@ -8,24 +10,23 @@ import ru.netology.patient.repository.PatientInfoFileRepository;
 import ru.netology.patient.repository.PatientInfoRepository;
 import ru.netology.patient.service.alert.SendAlertService;
 import ru.netology.patient.service.alert.SendAlertServiceImpl;
-import ru.netology.patient.service.medical.MedicalService;
-import ru.netology.patient.service.medical.MedicalServiceImpl;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class MedicalServiceTest {
+public class MedicalServiceImplTest {
 
-    ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
-    SendAlertService alertService;
-    MedicalService medicalService;
+    //    ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+    private SendAlertService alertService;
+    private MedicalService medicalService;
     PatientInfoRepository patientInfoRepository;
-    String id1 = "1";
-    String message = "Warning, patient with id: null, need help";
-    BloodPressure highPressure = new BloodPressure(150, 90);
-    BloodPressure normalPressure = new BloodPressure(120, 80);
-    BigDecimal highTemperature = new BigDecimal("40.0");
-    BigDecimal normalTemperature = new BigDecimal("36.6");
+    private final String id1 = "1";
+    private final String message = "Warning, patient with id: null, need help";
+    private final BloodPressure highPressure = new BloodPressure(150, 90);
+    private final BloodPressure normalPressure = new BloodPressure(120, 80);
+    private final BigDecimal highTemperature = new BigDecimal("40.0");
+    private final BigDecimal normalTemperature = new BigDecimal("36.6");
+
     @BeforeEach
     void initData() {
 
@@ -34,7 +35,6 @@ public class MedicalServiceTest {
         alertService = Mockito.spy(SendAlertServiceImpl.class);
         patientInfoRepository = Mockito.mock(PatientInfoFileRepository.class);
         Mockito.when(patientInfoRepository.getById(id1)).thenReturn(patientInfo);
-
         medicalService = new MedicalServiceImpl(patientInfoRepository, alertService);
     }
 
